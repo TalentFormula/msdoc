7b4127b9ba6ebc5204fcdfcb2bf166cb922fcdd1
AdvikB-TF <advik.b@gmail.com>
2025-09-08 13:55:13 +0530

    Create liststreams.go

---
+++ cmd/liststreams.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+    "fmt"
+    "os"
+
+    "github.com/TalentFormula/msdoc/ole2"
+)
+
+func main() {
+    if len(os.Args) < 2 {
+        fmt.Println("Usage: liststreams <file>")
+        os.Exit(1)
+    }
+    filePath := os.Args[1]
+    file, err := os.Open(filePath)
+    if err != nil {
+        fmt.Printf("Failed to open file: %v\n", err)
+        os.Exit(1)
+    }
+    defer file.Close()
+
+    reader, err := ole2.NewReader(file)
+    if err != nil {
+        fmt.Printf("Failed to create OLE2 reader: %v\n", err)
+        os.Exit(1)
+    }
+
+    streams := reader.ListStreams()
+    fmt.Println("Streams found:")
+    for _, s := range streams {
+        fmt.Printf("- '%s'\n", s)
+    }
+}
